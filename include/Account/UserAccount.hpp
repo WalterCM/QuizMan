1f5bf188f6513f8c83f015d7703cfdebe44c86b5
Walter Capa <walterc316@gmail.com>
2015-10-27 16:47:05 -0500

    Added Account classes the constants file

---
+++ include/Account/UserAccount.hpp
@@ -0,0 +1,14 @@
+#ifndef USERACCOUNT_HPP
+#define USERACCOUNT_HPP
+
+#include <QString>
+
+class UserAccount
+{
+public:
+    UserAccount();
+private:
+    QString accountName;
+};
+
+#endif // USERACCOUNT_HPP
---
+++ src/Account/UserAccount.cpp
@@ -0,0 +1,7 @@
+#include "include/Account/UserAccount.hpp"
+
+#include <QDebug>
+
+UserAccount::UserAccount()
+{
+}
